0d06583eb7b3926a8706c331933376018c9eb6b6
rshnewarray <rsh.newarray@gmail.com>
2021-09-22 15:33:21 +0800

    REKAYASA INTERNET

---
+++ app.py
@@ -0,0 +1,167 @@
+# Store this code in 'app.py' file
+
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import pymysql.cursors
+import os
+import re
+
+app = Flask(__name__)
+
+
+app.secret_key = 'your secret key'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'db_flask'
+
+mysql = MySQL(app)
+
+@app.route('/test')
+def test():
+    data = '1'
+    return render_template('base.html', data=data)
+
+#@app.route('/')
+@app.route('/', methods =['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE username = % s AND password = % s', (username, password, ))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            msg = 'Logged in successfully !'
+            # return render_template('index.html', msg = msg)
+            return redirect(url_for('index'))
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg = msg)
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE username = % s', (username, ))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers !'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form !'
+        else:
+            cursor.execute('INSERT INTO user VALUES (NULL, % s, % s, % s)', (username, email, password, ))
+            mysql.connection.commit()
+            msg = 'You have successfully registered !'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('register.html', msg = msg)
+
+conn = cursor = None
+#fungsi koneksi database
+
+
+def openDb():
+   global conn, cursor
+   conn = pymysql.connect(host='localhost', port=3306,
+                          user='root', passwd='', db='db_flask')
+   cursor = conn.cursor()
+#fungsi untuk menutup koneksi
+
+
+def closeDb():
+   global conn, cursor
+   cursor.close()
+   conn.close()
+#fungsi view index() untuk menampilkan data dari database
+
+
+@app.route('/barang')
+def index():
+   openDb()
+   container = []
+   sql = "SELECT * FROM barang"
+   cursor.execute(sql)
+   results = cursor.fetchall()
+   for data in results:
+      container.append(data)
+   closeDb()
+   return render_template('index.html', container=container,)
+
+#fungsi view tambah() untuk membuat form tambah
+
+
+@app.route('/tambah', methods=['GET', 'POST'])
+def tambah():
+   if request.method == 'POST':
+      nama = request.form['nama']
+      # nama = int(nama)
+      print(nama, request.form['nama'])
+      harga = re.sub("\D", '', request.form['harga'])
+      stok = request.form['stok']
+      openDb()
+      sql = "INSERT INTO barang (nama_barang, harga,stok) VALUES (%s, %s, %s)"
+      val = (nama, harga, stok)
+      cursor.execute(sql, val)
+      conn.commit()
+      closeDb()
+      return redirect(url_for('index'))
+   else:
+      return render_template('tambah.html')
+#fungsi view edit() untuk form edit
+
+
+@app.route('/edit/<id_barang>', methods=['GET', 'POST'])
+def edit(id_barang):
+   openDb()
+   cursor.execute('SELECT * FROM barang WHERE id_barang=%s', (id_barang))
+   data = cursor.fetchone()
+   if request.method == 'POST':
+      id_barang = request.form['id_barang']
+      nama = request.form['nama']
+      harga = request.form['harga']
+      stok = request.form['stok']
+      sql = "UPDATE barang SET nama_barang=%s, harga=%s, stok=%s WHERE id_barang=%s"
+      val = (nama, harga, stok, id_barang)
+      cursor.execute(sql, val)
+      conn.commit()
+      closeDb()
+      return redirect(url_for('index'))
+   else:
+      closeDb()
+      return render_template('edit.html', data=data)
+#fungsi untuk menghapus data
+
+
+@app.route('/hapus/<id_barang>', methods=['GET', 'POST'])
+def hapus(id_barang):
+   openDb()
+   cursor.execute('DELETE FROM barang WHERE id_barang=%s', (id_barang,))
+   conn.commit()
+   closeDb()
+   return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+   app.run(debug=True)
---
+++ db_flask.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 20, 2021 at 12:46 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_flask`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_barang` int(11) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `stok` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id_barang`, `nama_barang`, `harga`, `stok`) VALUES
+(1, 'siswa 1', 1, 1),
+(2, 'siswa 1', 1, 1),
+(3, 'siswa 1', 1, 1),
+(4, 'siswa 1', 1, 1),
+(5, 'siswa 1', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password`) VALUES
+(1, 'admin', 'admin@mail.com', 'adminadmin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
